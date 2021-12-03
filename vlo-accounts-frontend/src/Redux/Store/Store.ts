236b71ed41fa01dc0a27bc062beb6942ec7f92a4
gkju <gkjuszczyk@gmail.com>
2021-12-04 00:46:42 +0100

    submodule fix

--- vlo-accounts-frontend/src/Redux/Store/Store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {authSlice} from "../Slices/Auth";
-
-const rootReducer = combineReducers({auth: authSlice.reducer});
-
-export default configureStore({reducer: rootReducer});
-
-export type RootState = ReturnType<typeof rootReducer>
